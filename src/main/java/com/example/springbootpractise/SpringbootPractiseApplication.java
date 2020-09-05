1fe034a12dbf7b2541830f6c23280fc438bbee6b
liufeixiang <liufeixiang@zhiketong.cn>
2020-09-05 23:14:02 +0800

    Scheduled Test

--- src/main/java/com/example/springbootpractise/SpringbootPractiseApplication.java
+++ src/main/java/com/example/springbootpractise/SpringbootPractiseApplication.java
@@ -2,8 +2,14 @@ package com.example.springbootpractise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+/**
+ * @EnableScheduling 开启定时任务配置
+ */
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootPractiseApplication {
 
 	public static void main(String[] args) {
