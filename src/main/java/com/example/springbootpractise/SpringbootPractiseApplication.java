7d8a5fd881c894a87050c458188c4f64e09f869f
liufeixiang <liufeixiang@zhiketong.cn>
2020-09-05 16:26:56 +0800

    搭建基本框架-来源springboot initial page

---
+++ src/main/java/com/example/springbootpractise/SpringbootPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPractiseApplication.class, args);
+	}
+
+}
