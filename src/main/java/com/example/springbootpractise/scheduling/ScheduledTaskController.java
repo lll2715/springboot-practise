268b2b27bc2f80fc186b5a1b4463df48de938e24
liufeixiang <liufeixiang@zhiketong.cn>
2020-09-22 14:30:47 +0800

    validator验证

--- src/main/java/com/example/springbootpractise/scheduling/ScheduledTaskController.java
+++ src/main/java/com/example/springbootpractise/scheduling/ScheduledTaskController.java
@@ -15,7 +15,7 @@ public class ScheduledTaskController {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 500000)
     public void reportCurrentTime(){
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
