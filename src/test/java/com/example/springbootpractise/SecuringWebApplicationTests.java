0bed5300e89f7efc3c77e04a860305e1c0415ce4
liufeixiang <liufeixiang@zhiketong.cn>
2020-09-23 11:39:30 +0800

    安全认证--test类

---
+++ src/test/java/com/example/springbootpractise/SecuringWebApplicationTests.java
@@ -0,0 +1,63 @@
+package com.example.springbootpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecuringWebApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void loginWithValidUserThenAuthenticated() throws Exception {
+		FormLoginRequestBuilder login = formLogin()
+			.user("1")
+			.password("1");
+
+		mockMvc.perform(login)
+			.andExpect(authenticated().withUsername("1"));
+	}
+
+	@Test
+	public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+		FormLoginRequestBuilder login = formLogin()
+			.user("invalid")
+			.password("invalidpassword");
+
+		mockMvc.perform(login)
+			.andExpect(unauthenticated());
+	}
+
+	@Test
+	public void accessUnsecuredResourceThenOk() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
+		mockMvc.perform(get("/hello"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrlPattern("**/login"));
+	}
+
+	@Test
+	@WithMockUser
+	public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk());
+	}
+}
