0a5ff43edcfb3dd2dbc536eaf2d49f1b4a7f550e
liufeixiang <liufeixiang@zhiketong.cn>
2020-10-09 18:23:21 +0800

    JPA H2自定义保存获取

--- src/main/java/com/example/springbootpractise/SpringbootPractiseApplication.java
+++ src/main/java/com/example/springbootpractise/SpringbootPractiseApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
@@ -27,6 +28,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories
 public class SpringbootPractiseApplication {
 
 	public static void main(String[] args) {
